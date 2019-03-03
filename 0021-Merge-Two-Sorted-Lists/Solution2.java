4a275bc749a8af6a409a2ec84e5992b148029252
code4wt <tianxiaobo.com@outlook.com>
2019-03-03 20:40:15 +0800

    fix error

--- 0021-Merge-Two-Sorted-Lists/Solution2.java
+++ 0021-Merge-Two-Sorted-Lists/Solution2.java
@@ -6,7 +6,7 @@
  */
 public class Solution2 {
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
         if(l1.val < l2.val){
