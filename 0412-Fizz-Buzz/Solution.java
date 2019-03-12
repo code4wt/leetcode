0ea70ad4e9dc4cf47ce91e7bae7f7f3e9a59dab5
code4wt <tianxiaobo.com@outlook.com>
2019-03-12 11:53:00 +0800

    Fizz Buzz

---
+++ 0412-Fizz-Buzz/Solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author Tian ZhongBo
+ * @date 2019-03-12 11:52:04
+ */
+public class Solution {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
